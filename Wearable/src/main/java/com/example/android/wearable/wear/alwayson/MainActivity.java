72550a3e5b8910d5e8ac59eaa51dcd0034c02000
Pablo Johnson <pablo.88j@gmail.com>
2016-04-22 17:14:11 -0500

    Traducir comentarios

--- Wearable/src/main/java/com/example/android/wearable/wear/alwayson/MainActivity.java
+++ Wearable/src/main/java/com/example/android/wearable/wear/alwayson/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends WearableActivity {
     /** Custom 'what' for Message sent to Handler. */
     private static final int MSG_UPDATE_SCREEN = 0;
 
-    /** Milliseconds between updates based on state. */
+    /** Milisegundos entre cada actualizacion basado en el estado. */
     private static final long ACTIVE_INTERVAL_MS = TimeUnit.SECONDS.toMillis(1);
     private static final long AMBIENT_INTERVAL_MS = TimeUnit.SECONDS.toMillis(20);
 
@@ -91,18 +91,18 @@ public class MainActivity extends WearableActivity {
 
 
     /**
-     * Since the handler (used in active mode) can't wake up the processor when the device is in
-     * ambient mode and undocked, we use an Alarm to cover ambient mode updates when we need them
-     * more frequently than every minute. Remember, if getting updates once a minute in ambient
-     * mode is enough, you can do away with the Alarm code and just rely on the onUpdateAmbient()
-     * callback.
+     * Ya que el handler (usado en modo activo) no puede despertar al procesador cuando el dispositivo
+     * esta en modo ambiente y "undocked", se usa un Alarm para cubrir las actualizaciones el modo ambiente
+     * cuando estas son mas frecuentes que un minuto.
+     * Recuerda, si una actualizacion por minuto en modo ambiente es suficiente puedes deshacerte de la alarma
+     * y hacerlo solo con el onUpdateAmbient() callback
      */
     private AlarmManager mAmbientStateAlarmManager;
     private PendingIntent mAmbientStatePendingIntent;
 
     /**
-     * This custom handler is used for updates in "Active" mode. We use a separate static class to
-     * help us avoid memory leaks.
+     * Este handler se encarga de actualizar en el "Active" mode. Se usa una clase estatica separada para evitar
+     * memory leaks
      */
     private final Handler mActiveModeUpdateHandler = new UpdateHandler(this);
 
@@ -113,6 +113,8 @@ public class MainActivity extends WearableActivity {
 
         setContentView(R.layout.activity_main);
 
+        //Setea que esta actividad debe quedar en pantalla cuando el sistema entre en modo ambiente. Por defecto esta en false y la actividad
+        //se para cuando el sistema entra en modo ambiente.
         setAmbientEnabled();
 
         mAmbientStateAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
@@ -125,7 +127,7 @@ public class MainActivity extends WearableActivity {
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
 
-        /** Determines whether watch is round or square and applies proper view. **/
+        /** Determina si el reloj es redondo o cuadrado y aplica la vista apropiada. **/
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -143,8 +145,8 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * This is mostly triggered by the Alarms we set in Ambient mode and informs us we need to
-     * update the screen (and process any data).
+     * Esto es llamado por la Alarma que seteamos en el modo ambiente. Nos informa que tenemos que
+     * actualizar la pantalla (y procesar alguna data).
      */
     @Override
     public void onNewIntent(Intent intent) {
@@ -167,7 +169,7 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * Prepares UI for Ambient view.
+     * Preparar la UI para el vista de Ambiente.
      */
     @Override
     public void onEnterAmbient(Bundle ambientDetails) {
@@ -181,13 +183,13 @@ public class MainActivity extends WearableActivity {
          */
         mAmbientDetails = ambientDetails;
 
-        /** Clears Handler queue (only needed for updates in active mode). */
+        /** Limpia la cola del handler (ya que solo se necesita en modo activo). */
         mActiveModeUpdateHandler.removeMessages(MSG_UPDATE_SCREEN);
 
         /**
-         * Following best practices outlined in WatchFaces API (keeping most pixels black,
-         * avoiding large blocks of white pixels, using only black and white,
-         * and disabling anti-aliasing anti-aliasing, etc.)
+         * Siguiendo las buenas practicas en WatchFaces API (mantener la mayoria de pixeles en negro,
+         * evitar grandes bloques de pixeles blancos, usar solo blanco y negro, y
+         * deshabilitar el antialias).
          */
         mStateTextView.setTextColor(Color.WHITE);
         mUpdateRateTextView.setTextColor(Color.WHITE);
@@ -203,14 +205,9 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * Updates UI in Ambient view (once a minute). Because we need to update UI sooner than that
-     * (every ~20 seconds), we also use an Alarm. However, since the processor is awake for this
-     * callback, we might as well call refreshDisplayAndSetNextUpdate() to update screen and reset
-     * the Alarm.
-     *
-     * If you are happy with just updating the screen once a minute in Ambient Mode (which will be
-     * the case a majority of the time), then you can just use this method and remove all
-     * references/code regarding Alarms.
+     * Actualiza la UI en el modo ambiente (una vez por minuto). Ya que necesitamos hacerlos mas seguido
+     * (cada 20 segundos), usamos una alarma. Ya que el procesador esta despierto para este callback
+     * llamamos al refreshDisplayAndSetNextUpdate() para actualizar la pantalla y resetear la alarma.
      */
     @Override
     public void onUpdateAmbient() {
@@ -221,14 +218,14 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * Prepares UI for Active view (non-Ambient).
+     * Prepara la UI para modo activo.
      */
     @Override
     public void onExitAmbient() {
         Log.d(TAG, "onExitAmbient()");
         super.onExitAmbient();
 
-        /** Clears out Alarms since they are only used in ambient mode. */
+        /** Borra las alarmas ya que ellas son necesarias solo en modo ambiente. */
         mAmbientStateAlarmManager.cancel(mAmbientStatePendingIntent);
 
         mStateTextView.setTextColor(Color.GREEN);
@@ -245,8 +242,8 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * Loads data/updates screen (via method), but most importantly, sets up the next refresh
-     * (active mode = Handler and ambient mode = Alarm).
+     * Carga/actualiza la dara en la pantalla y setea la proxima actualizacion
+     * (modo activo = Handler and modo ambiente = Alarm).
      */
     private void refreshDisplayAndSetNextUpdate() {
 
@@ -255,14 +252,14 @@ public class MainActivity extends WearableActivity {
         long timeMs = System.currentTimeMillis();
 
         if (isAmbient()) {
-            /** Calculate next trigger time (based on state). */
+            /** Calcula cuando sera la proxima llamada dependiendo el estado. */
             long delayMs = AMBIENT_INTERVAL_MS - (timeMs % AMBIENT_INTERVAL_MS);
             long triggerTimeMs = timeMs + delayMs;
 
             /**
-             * Note: Make sure you have set activity launchMode to singleInstance in the manifest.
-             * Otherwise, it is easy for the AlarmManager launch intent to open a new activity
-             * every time the Alarm is triggered rather than reusing this Activity.
+             * Nota: Asegurarse de setear a la actividad el launchMode a singleInstace en el manifest.
+             * De lo contrario el AlarmManager intentara abrir una nueva actividad cada vez que la alarma
+             * dispare un evento en vez de reusar la actividad actual.
              */
             mAmbientStateAlarmManager.setExact(
                     AlarmManager.RTC_WAKEUP,
@@ -270,7 +267,7 @@ public class MainActivity extends WearableActivity {
                     mAmbientStatePendingIntent);
 
         } else {
-            /** Calculate next trigger time (based on state). */
+            /** Calcula cuando sera la proxima llamada dependiendo el estado. */
             long delayMs = ACTIVE_INTERVAL_MS - (timeMs % ACTIVE_INTERVAL_MS);
 
             mActiveModeUpdateHandler.removeMessages(MSG_UPDATE_SCREEN);
@@ -279,7 +276,7 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * Updates display based on Ambient state. If you need to pull data, you should do it here.
+     * Actualiza la data basado en el stado ambiente. Si se necesita pedir data, se deberia hacer aca.
      */
     private void loadDataAndUpdateScreen() {
 
@@ -311,7 +308,7 @@ public class MainActivity extends WearableActivity {
     }
 
     /**
-     * Handler separated into static class to avoid memory leaks.
+     * Handler separado en una clase estatica para evitar memory leaks.
      */
     private static class UpdateHandler extends Handler {
         private final WeakReference<MainActivity> mMainActivityWeakReference;
